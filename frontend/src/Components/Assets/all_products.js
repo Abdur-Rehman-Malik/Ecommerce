ca15d4e70c5bffff07bb316d657c097561c8db6a
Abdur-Rehman-Malik <abdurrehmanm849@gmail.com>
2024-07-13 03:23:43 +0500

    pictures added

---
+++ frontend/src/Components/Assets/all_products.js
@@ -0,0 +1,327 @@
+import p1_img from "./product_1.jpg";
+import p2_img from "./product_2.jpg";
+import p3_img from "./product_3.jpg";
+import p4_img from "./product_4.jpg";
+import p5_img from "./product_5.jpg";
+import p6_img from "./product_6.jpg";
+import p7_img from "./product_7.jpg";
+import p8_img from "./product_8.jpg";
+import p9_img from "./product_9.jpg";
+import p10_img from "./product_10.jpg";
+import p11_img from "./product_11.jpg";
+import p12_img from "./product_12.jpg";
+import p13_img from "./product_13.jpg";
+import p14_img from "./product_14.jpg";
+import p15_img from "./product_15.jpg";
+import p16_img from "./product_16.jpg";
+import p17_img from "./product_17.jpg";
+import p18_img from "./product_18.jpg";
+import p19_img from "./product_19.jpg";
+import p20_img from "./product_20.jpg";
+import p21_img from "./product_21.jpg";
+import p22_img from "./product_22.jpg";
+import p23_img from "./product_23.jpg";
+import p24_img from "./product_24.jpg";
+import p25_img from "./product_25.jpg";
+import p26_img from "./product_26.jpg";
+import p27_img from "./product_27.jpg";
+import p28_img from "./product_28.jpg";
+import p29_img from "./product_29.jpg";
+import p30_img from "./product_30.jpg";
+import p31_img from "./product_31.jpg";
+import p32_img from "./product_32.jpg";
+import p33_img from "./product_33.jpg";
+import p34_img from "./product_34.jpg";
+import p35_img from "./product_35.jpg";
+import p36_img from "./product_36.jpg";
+
+let all_products = [
+  {
+    id: 1,
+    name: "Men's Casual T-Shirt 1",
+    category: "men",
+    image: p1_img,
+    new_price: 1500,
+    old_price: 2000,
+  },
+  {
+    id: 2,
+    name: "Men's Casual T-Shirt 2",
+    category: "men",
+    image: p2_img,
+    new_price: 1600,
+    old_price: 2100,
+  },
+  {
+    id: 3,
+    name: "Men's Casual T-Shirt 3",
+    category: "men",
+    image: p3_img,
+    new_price: 1700,
+    old_price: 2200,
+  },
+  {
+    id: 4,
+    name: "Men's Casual T-Shirt 4",
+    category: "men",
+    image: p4_img,
+    new_price: 1800,
+    old_price: 2300,
+  },
+  {
+    id: 5,
+    name: "Men's Casual T-Shirt 5",
+    category: "men",
+    image: p5_img,
+    new_price: 1900,
+    old_price: 2400,
+  },
+  {
+    id: 6,
+    name: "Men's Casual T-Shirt 6",
+    category: "men",
+    image: p6_img,
+    new_price: 2000,
+    old_price: 2500,
+  },
+  {
+    id: 7,
+    name: "Men's Casual T-Shirt 7",
+    category: "men",
+    image: p7_img,
+    new_price: 2100,
+    old_price: 2600,
+  },
+  {
+    id: 8,
+    name: "Men's Casual T-Shirt 8",
+    category: "men",
+    image: p8_img,
+    new_price: 2200,
+    old_price: 2700,
+  },
+  {
+    id: 9,
+    name: "Men's Casual T-Shirt 9",
+    category: "men",
+    image: p9_img,
+    new_price: 2300,
+    old_price: 2800,
+  },
+  {
+    id: 10,
+    name: "Women's Casual T-Shirt 1",
+    category: "women",
+    image: p10_img,
+    new_price: 2500,
+    old_price: 3000,
+  },
+  {
+    id: 11,
+    name: "Women's Casual T-Shirt 2",
+    category: "women",
+    image: p11_img,
+    new_price: 2600,
+    old_price: 3100,
+  },
+  {
+    id: 12,
+    name: "Women's Casual T-Shirt 3",
+    category: "women",
+    image: p12_img,
+    new_price: 2700,
+    old_price: 3200,
+  },
+  {
+    id: 13,
+    name: "Women's Casual T-Shirt 4",
+    category: "women",
+    image: p13_img,
+    new_price: 2800,
+    old_price: 3300,
+  },
+  {
+    id: 14,
+    name: "Women's Casual T-Shirt 5",
+    category: "women",
+    image: p14_img,
+    new_price: 2900,
+    old_price: 3400,
+  },
+  {
+    id: 15,
+    name: "Women's Casual T-Shirt 6",
+    category: "women",
+    image: p15_img,
+    new_price: 3000,
+    old_price: 3500,
+  },
+  {
+    id: 16,
+    name: "Women's Casual T-Shirt 7",
+    category: "women",
+    image: p16_img,
+    new_price: 3100,
+    old_price: 3600,
+  },
+  {
+    id: 17,
+    name: "Women's Casual T-Shirt 8",
+    category: "women",
+    image: p17_img,
+    new_price: 3200,
+    old_price: 3700,
+  },
+  {
+    id: 18,
+    name: "Women's Casual T-Shirt 9",
+    category: "women",
+    image: p18_img,
+    new_price: 3300,
+    old_price: 3800,
+  },
+  {
+    id: 19,
+    name: "Kids' Casual T-Shirt 1",
+    category: "kids",
+    image: p19_img,
+    new_price: 800,
+    old_price: 1200,
+  },
+  {
+    id: 20,
+    name: "Kids' Casual T-Shirt 2",
+    category: "kids",
+    image: p20_img,
+    new_price: 900,
+    old_price: 1300,
+  },
+  {
+    id: 21,
+    name: "Kids' Casual T-Shirt 3",
+    category: "kids",
+    image: p21_img,
+    new_price: 1000,
+    old_price: 1400,
+  },
+  {
+    id: 22,
+    name: "Kids' Casual T-Shirt 4",
+    category: "kids",
+    image: p22_img,
+    new_price: 1100,
+    old_price: 1500,
+  },
+  {
+    id: 23,
+    name: "Kids' Casual T-Shirt 5",
+    category: "kids",
+    image: p23_img,
+    new_price: 1200,
+    old_price: 1600,
+  },
+  {
+    id: 24,
+    name: "Kids' Casual T-Shirt 6",
+    category: "kids",
+    image: p24_img,
+    new_price: 1300,
+    old_price: 1700,
+  },
+  {
+    id: 25,
+    name: "Kids' Casual T-Shirt 7",
+    category: "kids",
+    image: p25_img,
+    new_price: 1400,
+    old_price: 1800,
+  },
+  {
+    id: 26,
+    name: "Kids' Casual T-Shirt 8",
+    category: "kids",
+    image: p26_img,
+    new_price: 1500,
+    old_price: 1900,
+  },
+  {
+    id: 27,
+    name: "Kids' Casual T-Shirt 9",
+    category: "kids",
+    image: p27_img,
+    new_price: 1600,
+    old_price: 2000,
+  },
+  {
+    id: 28,
+    name: "New Collection T-Shirt 1",
+    category: "new",
+    image: p28_img,
+    new_price: 2500,
+    old_price: 3000,
+  },
+  {
+    id: 29,
+    name: "New Collection T-Shirt 2",
+    category: "new",
+    image: p29_img,
+    new_price: 2600,
+    old_price: 3100,
+  },
+  {
+    id: 30,
+    name: "New Collection T-Shirt 3",
+    category: "new",
+    image: p30_img,
+    new_price: 2700,
+    old_price: 3200,
+  },
+  {
+    id: 31,
+    name: "New Collection T-Shirt 4",
+    category: "new",
+    image: p31_img,
+    new_price: 2800,
+    old_price: 3300,
+  },
+  {
+    id: 32,
+    name: "New Collection T-Shirt 5",
+    category: "new",
+    image: p32_img,
+    new_price: 2900,
+    old_price: 3400,
+  },
+  {
+    id: 33,
+    name: "New Collection T-Shirt 6",
+    category: "new",
+    image: p33_img,
+    new_price: 3000,
+    old_price: 3500,
+  },
+  {
+    id: 34,
+    name: "New Collection T-Shirt 7",
+    category: "new",
+    image: p34_img,
+    new_price: 3100,
+    old_price: 3600,
+  },
+  {
+    id: 35,
+    name: "New Collection T-Shirt 8",
+    category: "new",
+    image: p35_img,
+    new_price: 3200,
+    old_price: 3700,
+  },
+  {
+    id: 36,
+    name: "New Collection T-Shirt 9",
+    category: "new",
+    image: p36_img,
+    new_price: 3300,
+    old_price: 3800,
+  },
+];
